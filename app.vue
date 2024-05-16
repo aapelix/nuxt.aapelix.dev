5ba2fc1016dfaabb7ef1ab7a83c6615daebd0fba
aapelix <mcaapelix@gmail.com>
2024-05-16 17:38:44 +0300

    Nuxt aapelix.dev

---
+++ app.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Site Title` : 'Site Title';
+  }
+})
+</script>
+
+<template>
+  <div class="font-mono bg-[#0f0f0f] w-screen h-screen text-[#f3f3f3] overflow-x-hidden">
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+  .page-enter-active,
+  .page-leave-active {
+    transition: all 0.4s;
+  }
+
+  .page-enter-from,
+  .page-leave-to {
+    transform: translateY(-20px);
+    filter: blur(1rem);
+  }
+
+  :root {
+    scroll-behavior: smooth;
+    font-family: "Noto Sans Mono", monospace;
+    background: #0f0f0f;
+  }
+
+  * {
+    -ms-overflow-style: none;
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <div class="flex justify-center items-center h-screen font-black">
+            <h1 class="text-6xl">aapelix.dev</h1>
+        </div>
+        <div class="w-[50vw] h-[50vw] bg-[#e0e0e0] absolute -bottom-[80%] left-1/2 -translate-x-1/2 rounded-full blur-[200px]" />
+    </div>
+</template>
